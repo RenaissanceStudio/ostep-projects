b0303c67e5abdf9995f80aafc5c18c9cad283f3e
rayworks <crayzhou@gmail.com>
2021-12-22 15:47:07 +0800

    Build a simple multi-threaded web server

--- concurrency-webserver/src/spin.c
+++ concurrency-webserver/src/spin.c
@@ -17,7 +17,7 @@ double get_seconds() {
     struct timeval t;
     int rc = gettimeofday(&t, NULL);
     assert(rc == 0);
-    return (double) ((double)t.tv_sec + (double)t.tv_usec / 1e6);
+    return (double) ((double) t.tv_sec + (double) t.tv_usec / 1e6);
 }
 
 
@@ -26,27 +26,27 @@ int main(int argc, char *argv[]) {
     double spin_for = 0.0;
     char *buf;
     if ((buf = getenv("QUERY_STRING")) != NULL) {
-	// just expecting a single number
-	spin_for = (double) atoi(buf);
+        // just expecting a single number
+        spin_for = (double) atoi(buf);
     }
 
     double t1 = get_seconds();
     while ((get_seconds() - t1) < spin_for)
-	sleep(1);
+        sleep(1);
     double t2 = get_seconds();
-    
+
     /* Make the response body */
     char content[MAXBUF];
     sprintf(content, "<p>Welcome to the CGI program (%s)</p>\r\n", buf);
     sprintf(content, "%s<p>My only purpose is to waste time on the server!</p>\r\n", content);
     sprintf(content, "%s<p>I spun for %.2f seconds</p>\r\n", content, t2 - t1);
-    
+
     /* Generate the HTTP response */
     printf("Content-Length: %lu\r\n", strlen(content));
     printf("Content-Type: text/html\r\n\r\n");
     printf("%s", content);
     fflush(stdout);
-    
+
     exit(0);
 }
 
--- concurrency-webserver/src/wserver.c
+++ concurrency-webserver/src/wserver.c
@@ -1,47 +1,138 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
 #include "request.h"
 #include "io_helper.h"
+#include "include/common_threads.h"
 
 char default_root[] = ".";
 
+pthread_cond_t empty, fill;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+int buf_sz = 1;
+int *buffer; // to be allocated on the fly
+
+int worker_cnt = 1;
+
+int fill_ptr = 0;
+int use_ptr = 0;
+
+/* Size of connected fd buffer */
+int count = 0;
+
+void create_workers();
+
+void print_buf() {
+    int *pt = buffer;
+    for (int i = 0; i < count; ++i) {
+        printf(" | %d ", *(pt + i));
+        ++pt;
+    }
+    printf("| filled pos : %d | use pos :  %d\n", fill_ptr, use_ptr);
+}
+
+void add_connected_fd(int fd) {
+    buffer[fill_ptr] = fd;
+    fill_ptr = (fill_ptr + 1) % buf_sz;
+    count++;
+}
+
+int get_connected_fd() {
+    int tmp = buffer[use_ptr];
+    use_ptr = (use_ptr + 1) % buf_sz;
+    count--;
+    return tmp;
+}
+
+void add_task(int fd) {
+    Pthread_mutex_lock(&mutex);
+    while (count == buf_sz)
+        Pthread_cond_wait(&empty, &mutex)
+    add_connected_fd(fd);
+    Pthread_cond_signal(&fill);
+    Pthread_mutex_unlock(&mutex);
+}
+
+void *handle_request(void *args) {
+    int index = *((int *) args);
+    printf(">>> thread index : %d\n", index);
+    while (1) {
+        Pthread_mutex_lock(&mutex);
+        while (0 == count)
+            Pthread_cond_wait(&fill, &mutex);
+        int fd = get_connected_fd();
+        Pthread_cond_signal(&empty);
+        Pthread_mutex_unlock(&mutex);
+
+        printf(">>> thread index : %d is working now with fd : %d \n", index, fd);
+        request_handle(fd);
+        close_or_die(fd);
+        printf(">>> thread index : %d is idle\n", index);
+    }
+
+    return NULL;
+}
+
 //
-// ./wserver [-d <basedir>] [-p <portnum>] 
+// ./wserver [-d <basedir>] [-p <portnum>] [-t threads] [-b buffers]
 // 
 int main(int argc, char *argv[]) {
     int c;
     char *root_dir = default_root;
     int port = 10000;
-    
-    while ((c = getopt(argc, argv, "d:p:")) != -1)
-	switch (c) {
-	case 'd':
-	    root_dir = optarg;
-	    break;
-	case 'p':
-	    port = atoi(optarg);
-	    break;
-	default:
-	    fprintf(stderr, "usage: wserver [-d basedir] [-p port]\n");
-	    exit(1);
-	}
+
+    while ((c = getopt(argc, argv, "d:p:t:b:")) != -1)
+        switch (c) {
+            case 'd':
+                root_dir = optarg;
+                break;
+            case 'p':
+                port = atoi(optarg);
+                break;
+            case 't':
+                worker_cnt = atoi(optarg);
+                break;
+            case 'b':
+                buf_sz = atoi(optarg);
+                buffer = (int *) malloc(sizeof(int) * buf_sz);
+                break;
+            default:
+                fprintf(stderr, "usage: wserver [-d basedir] [-p port]\n");
+                exit(1);
+        }
 
     // run out of this directory
     chdir_or_die(root_dir);
 
     // now, get to work
     int listen_fd = open_listen_fd_or_die(port);
+
+    create_workers();
+
     while (1) {
-	struct sockaddr_in client_addr;
-	int client_len = sizeof(client_addr);
-	int conn_fd = accept_or_die(listen_fd, (sockaddr_t *) &client_addr, (socklen_t *) &client_len);
-	request_handle(conn_fd);
-	close_or_die(conn_fd);
+        struct sockaddr_in client_addr;
+        int client_len = sizeof(client_addr);
+        int conn_fd = accept_or_die(listen_fd, (sockaddr_t *) &client_addr, (socklen_t * ) & client_len);
+
+        add_task(conn_fd);
     }
     return 0;
 }
 
+void create_workers() {
+    if (!buffer) {
+        buffer = (int *) malloc(sizeof(int));
+    }
+    pthread_cond_init(&empty, NULL);
+    pthread_cond_init(&fill, NULL);
 
-    
+    pthread_t pthread;
+    for (int i = 0; i < worker_cnt; ++i) {
+        int *pval = malloc(sizeof(int));
+        *pval = i;
 
+        Pthread_create(&pthread, NULL, handle_request, pval);
+    }
+}
 
- 
